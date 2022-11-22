5288ada457f78391d5c91a646715388d6d7d28ec
mgalcuadrado <110743194+mgalcuadrado@users.noreply.github.com>
2022-11-22 20:39:31 -0300

    Update config.h
    Signed-off-by: mgalcuadrado <110743194+mgalcuadrado@users.noreply.github.com>

--- config.h
+++ config.h
@@ -4,7 +4,6 @@
 #define CANTIDAD_TESELAS 4096
 #define CANTIDAD_FIGURAS 13
 #define CANTIDAD_RUTAS 4
-
 #define CANTIDAD_ROMS 18
 
 #define ANCHO_TESELA 8
@@ -21,6 +20,27 @@
 
 #define JUEGO_FPS 30
 
+#define ARCHIVO_ROM_R   "roms/6841.rom"
+#define ARCHIVO_ROM_G   "roms/6842.rom"
+#define ARCHIVO_ROM_B   "roms/6843.rom"
+#define ARCHIVO_ROM_RUTA "roms/6840.rom"
+#define ARCHIVO_ROM_FIGURA_1 "roms/6819.rom"
+#define ARCHIVO_ROM_FIGURA_2 "roms/6820.rom"
+#define ARCHIVO_ROM_FIGURA_3 "roms/6821.rom"
+#define ARCHIVO_ROM_FIGURA_4 "roms/6822.rom"
+#define ARCHIVO_ROM_FIGURA_5 "roms/6823.rom"
+#define ARCHIVO_ROM_FIGURA_6 "roms/6824.rom"
+#define ARCHIVO_ROM_FIGURA_7 "roms/6825.rom"
+#define ARCHIVO_ROM_FIGURA_8 "roms/6826.rom"
+#define ARCHIVO_ROM_FIGURA_9 "roms/6827.rom"
+#define ARCHIVO_ROM_FIGURA_10 "roms/6828.rom"
+#define ARCHIVO_ROM_FIGURA_11 "roms/6829.rom"
+#define ARCHIVO_ROM_FIGURA_12 "roms/6830.rom"
+#define ARCHIVO_ROM_FIGURA_13 "roms/6845.rom"
+#define ARCHIVO_ROM_FIGURA_14 "roms/6846.rom"
+
+
+/*
 typedef enum{
     ARCHIVO_ROM_B,
     ARCHIVO_ROM_G,
@@ -42,7 +62,7 @@ typedef enum{
     ARCHIVO_ROM_FIGURA_14,
 }archivos_t;
 
-const char * archivos_rom[CANTIDAD_ROMS] = {  
+const char *archivos_rom[CANTIDAD_ROMS] = {  
 [ARCHIVO_ROM_R] = "roms/6841.rom",
 [ARCHIVO_ROM_G] = "roms/6842.rom",
 [ARCHIVO_ROM_B] = "roms/6843.rom",
@@ -62,6 +82,5 @@ const char * archivos_rom[CANTIDAD_ROMS] = {
 [ARCHIVO_ROM_FIGURA_13] = "roms/6845.rom",
 [ARCHIVO_ROM_FIGURA_14] = "roms/6846.rom",
 };
-
-
+*/
 #endif
